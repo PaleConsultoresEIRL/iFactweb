4e1ec76ecc5b52798cde26f56b349f8e557cc5b6
CxbxC <lsaire97@gmail.com>
2021-01-21 19:54:35 -0500

    web

---
+++ precache-manifest.2b6e35be71eeeab842f9d50390d65e1a.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7155ad92f29f064b891f5f4504e229e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b677e9e4adc28c2b72f",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "4967575b4a67a0f19f6f",
+    "url": "/static/css/main.1b8e515e.chunk.css"
+  },
+  {
+    "revision": "9b677e9e4adc28c2b72f",
+    "url": "/static/js/2.d43e50f6.chunk.js"
+  },
+  {
+    "revision": "d3fe802f521069c533c2d93f4d5bace1",
+    "url": "/static/js/2.d43e50f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4967575b4a67a0f19f6f",
+    "url": "/static/js/main.8dc45368.chunk.js"
+  },
+  {
+    "revision": "d57df049333bcb5c1737",
+    "url": "/static/js/runtime-main.2449dd0f.js"
+  },
+  {
+    "revision": "8afa1dc82cb11a25f87a0b52ded6ccbb",
+    "url": "/static/media/Hero.8afa1dc8.png"
+  },
+  {
+    "revision": "6b441db8b8d560559e0d7c7b3cacc38a",
+    "url": "/static/media/Logo_720.6b441db8.png"
+  },
+  {
+    "revision": "b885bb584d0bf0b16e02554d46496d25",
+    "url": "/static/media/Logo_monocromático blanco720.b885bb58.png"
+  },
+  {
+    "revision": "403936e15eedb8451f02a79a36c4e46a",
+    "url": "/static/media/Logo_negativo720.403936e1.png"
+  },
+  {
+    "revision": "65ffa2913364c7bb574c37a4ebfc7cb3",
+    "url": "/static/media/MigracióndeBasedeDatosaMongoDB.65ffa291.pdf"
+  },
+  {
+    "revision": "b520399319ef612cecbcb3eb95edcf6d",
+    "url": "/static/media/SolutionBackground.b5203993.png"
+  },
+  {
+    "revision": "b9951ea536d39b0f4e32edddf506cd23",
+    "url": "/static/media/Web12802.b9951ea5.png"
+  },
+  {
+    "revision": "f4bbc4abdd0ef43296ab74db1d04233e",
+    "url": "/static/media/consulta-400.f4bbc4ab.jpg"
+  },
+  {
+    "revision": "4c3e312b05bf4a5265352cf4d3a7db4f",
+    "url": "/static/media/electronica.4c3e312b.jpg"
+  },
+  {
+    "revision": "abbcc800caa174b46601b7b5e1fdb4e7",
+    "url": "/static/media/facturacion.abbcc800.png"
+  },
+  {
+    "revision": "5438b416a48fd801d122f2ee0aa4c906",
+    "url": "/static/media/farmacia-200x200.5438b416.png"
+  },
+  {
+    "revision": "7c258462d0bade6e4a88452d62dae34b",
+    "url": "/static/media/fechaEmision.7c258462.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3a585142bdb16e0909d01d56146d1d48",
+    "url": "/static/media/gracias.3a585142.jpg"
+  },
+  {
+    "revision": "93e345a71291d483db657a38c65dd6a0",
+    "url": "/static/media/grifos-200x200.93e345a7.png"
+  },
+  {
+    "revision": "952886a56bc21b99e35e2c6d35927059",
+    "url": "/static/media/hoteless-200x200.952886a5.png"
+  },
+  {
+    "revision": "1402741e0b783689596da95704afb7fa",
+    "url": "/static/media/img2.1402741e.jpg"
+  },
+  {
+    "revision": "15f43bc872b7c891eb67249fdcb46093",
+    "url": "/static/media/medio-ambiente.15f43bc8.jpg"
+  },
+  {
+    "revision": "7c17abad06a510d249c6065f78d6719a",
+    "url": "/static/media/numero.7c17abad.png"
+  },
+  {
+    "revision": "df38358dfa64d7a22597e6ff20d05672",
+    "url": "/static/media/relojArena.df38358d.jpg"
+  },
+  {
+    "revision": "ea70d2515baed35ef861d350fb6a55bd",
+    "url": "/static/media/restaurantes-200x200.ea70d251.png"
+  },
+  {
+    "revision": "ae3ef6c738e1f7ab4553d36b0cecea1d",
+    "url": "/static/media/ruc.ae3ef6c7.png"
+  },
+  {
+    "revision": "67c7bd620a58dc1222b59ddfa2a73b34",
+    "url": "/static/media/serie.67c7bd62.png"
+  },
+  {
+    "revision": "10a929b54314718247a73af568b64a50",
+    "url": "/static/media/stores-200x200.10a929b5.png"
+  },
+  {
+    "revision": "38208f3154a7c0ec9dcfc4d92115d376",
+    "url": "/static/media/telecomunicaciones-200x200.38208f31.png"
+  },
+  {
+    "revision": "c48d846c9c7098d548ec830bd638879d",
+    "url": "/static/media/tipoComprobante.c48d846c.png"
+  }
+]);
